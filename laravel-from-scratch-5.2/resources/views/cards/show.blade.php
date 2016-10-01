fa4bf7c632178064c6d1a5c0f68f01726d2839ca
Marko Burazin <marko.burazin1@gmail.com>
2016-10-01 22:17:22 +0200

    Laravel From Scratch: 13 - Authenticate your users
    Added user authorization scaffolding via: "php artisan make:auth" with a few minor adjustments to the existing webpage.
    Changed username -> name in the users table.

--- laravel-from-scratch-5.2/resources/views/cards/show.blade.php
+++ laravel-from-scratch-5.2/resources/views/cards/show.blade.php
@@ -10,7 +10,7 @@
             @foreach ($card->notes as $note)
                <li class="list-group-item">
                   <a href="/notes/{{ $note->id }}/edit">{{ $note->body }}</a>
-                  <a href="#" class="pull-right">{{ $note->user->username }}</a>
+                  <a href="#" class="pull-right">{{ $note->user->name }}</a>
                </li>
             @endforeach
          </ul>
