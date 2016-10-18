d0586c7a5647962b096b045aa8b808c624ce7eee
Marko Burazin <marko.burazin1@gmail.com>
2016-10-18 23:22:55 +0200

    1st part of Task list app (quickstart)
    1) Prepping the database
    2) Routing (incl. authentication)
    3) Building layouts and Views
    
    "Adding Tasks" soon to follow...

---
+++ laravel/quickstart/app/Http/Controllers/TaskController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class TaskController extends Controller
+{
+    public function __construct()
+    {
+    	$this->middleware('auth');
+    }
+
+    public function index(Request $request)
+    {
+        return view(tasks.index);
+    }
+}
